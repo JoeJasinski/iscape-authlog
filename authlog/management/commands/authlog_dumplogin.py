d72e43fa7330afe5ea219a9e61e29c251f6f584b
jjasinski <jjasinski@a564d3b4-0141-4fa4-8e6d-0c141b018941>
2010-10-21 16:22:09 +0000

    created management commands to dump data to CSV file
    git-svn-id: svn+ssh://svn.imagescape.com/export/authlog/trunk@20 a564d3b4-0141-4fa4-8e6d-0c141b018941

---
+++ authlog/management/commands/authlog_dumplogin.py
@@ -0,0 +1,32 @@
+import sys
+import csv
+import unicodedata
+from django.core.management.base import BaseCommand, CommandError
+from authlog.models import Access
+
+CSV_HEADERS=['login_time', 'ip_address', 'user', 'path_info', 'user_agent',
+             'get_data', 'post_data', 'http_accept', ]
+
+class Command(BaseCommand):
+    args = 'None'
+    help = 'Dump the Authentication Log to stdout in CSV format.'
+
+    def safe_unicode(self, input_string):
+        return unicode(unicodedata.normalize('NFKD',input_string).encode('ascii','ignore'))
+
+    def handle(self, *args, **options):
+
+        csv_writer = csv.writer(sys.stdout)
+        csv_writer.writerow(CSV_HEADERS)
+        for record in Access.objects.all():
+            csv_writer.writerow([
+                record.login_time,                                
+                self.safe_unicode(record.ip_address),
+                self.safe_unicode(record.user),
+                self.safe_unicode(record.path_info),
+                self.safe_unicode(record.user_agent),
+                self.safe_unicode(record.get_data),
+                self.safe_unicode(record.post_data),
+                self.safe_unicode(record.http_accept),
+            ])
+
