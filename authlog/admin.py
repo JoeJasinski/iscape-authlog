0d60d5d46876a8a33fa6e0a9dc440b2755056d36
jjasinski <jjasinski@a564d3b4-0141-4fa4-8e6d-0c141b018941>
2010-10-21 15:18:53 +0000

    removed user agent
    git-svn-id: svn+ssh://svn.imagescape.com/export/authlog/trunk@19 a564d3b4-0141-4fa4-8e6d-0c141b018941

--- authlog/admin.py
+++ authlog/admin.py
@@ -39,7 +39,7 @@ class ReadOnlyAdminFields(object):
 
 
 class AccessAdmin(ReadOnlyAdminFields, admin.ModelAdmin):
-    list_display = ('user','login_time', 'ip_address', 'user_agent', 'path_info', )
+    list_display = ('user','login_time', 'ip_address','path_info', )
     list_filter = ['user','login_time', 'ip_address', ]
     search_fields = ['user','ip_address', 'user_agent', 'path_info']
     date_hierarchy = 'login_time'
@@ -67,7 +67,7 @@ class AccessAdmin(ReadOnlyAdminFields, admin.ModelAdmin):
 
 
 class AccessPageAdmin(ReadOnlyAdminFields, admin.ModelAdmin):
-    list_display = ('user','access_time', 'ip_address', 'user_agent', 'path_info', )
+    list_display = ('user','access_time', 'ip_address', 'path_info', )
     list_filter = ['user','access_time', 'ip_address', ]
     search_fields = ['user','ip_address', 'user_agent', 'path_info']
     date_hierarchy = 'access_time'
