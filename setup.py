c79766ea1592ab68f37cb593852f5e00a650b755
Joe Jasinski <joe.jasinski@gmail.com>
2011-06-20 22:23:31 -0700

    Edited setup.py via GitHub

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "Authlog",
-        version = "0.1",
+        version = "0.2",
         author = "Joe Jasinski",
         author_email = "jjasinski@imagescape.com",
         url = "http://www.imagescape.com",
